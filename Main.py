a15fe28219b428dff4f96ee5c3328510e6497132
Tara URK21CS1022 <100012608+Tara-1022@users.noreply.github.com>
2022-09-05 15:48:09 +0530

    Initial Commit

--- Main.py
+++ Main.py
@@ -7,16 +7,25 @@ class Node:
 class Queue:
   def __init__(self):
     self.head = None
-    self.last = None
+    self.tail = None
 
   def enqueue(self, data) -> None:
-    # Write your code here
+    new = Node(data)
+    if not self.tail is None:
+     self.tail.next = new
+    self.tail = new
 
   def dequeue(self) -> None:
-    # Write your code here
+    if not self.head is None:
+      self.head = self.head.next
 
   def status(self) -> None:
-    # Write your code here
+    elements = ""
+    curr = self.head
+    while not curr is None:
+      elements += str(curr.data) + "=>"
+      curr = curr.next
+    print(elements + str(curr))
 
 
 # Do not change the following code
